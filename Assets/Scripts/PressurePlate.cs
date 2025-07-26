0c4861c37f7dc25c59bdf7cc83785968b7148fde
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2025-07-27 01:40:32 +0700

    0.0.1 Interaction Events Updates

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public InteractionEvent targetEvent;
+    public string requiredTag = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"PressurePlate triggered by: {other.name}");
+        if (other.CompareTag(requiredTag))
+            targetEvent?.Interact();
+    }
+}
